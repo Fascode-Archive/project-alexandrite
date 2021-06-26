fb70f21148c63e2ded6875ca3a7ed7d76c99dced
nexryai <nexryai@gmail.com>
2021-06-26 17:00:23 +0900

    powerlineを導入

--- alexandrite/config.sh
+++ alexandrite/config.sh
@@ -1,44 +1,46 @@
-#!/bin/bash
-#================
-# FILE          : config.sh
-#----------------
-# PROJECT       : alexandrite linux project
-# COPYRIGHT     : (c) 2021 naiad technology
-#               :
-# AUTHOR        : nexryai
-#               :
-# BELONGS TO    : Operating System images
-#               :
-# DESCRIPTION   : configuration script for SUSE based
-#               : operating systems
-#----------------
-#======================================
+#! /usr/bin/env bash
+
+#    Alexandrite OS
+#    Copyright (C) 2021 naiad technology
+#
+#
+#    config.sh
+#
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # Functions...
-#--------------------------------------
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-#======================================
 # Greeting...
-#--------------------------------------
 echo "Configure image: [$kiwi_iname]..."
 
-#======================================
 # Setup baseproduct link
-#--------------------------------------
 suseSetupProduct
 
-#======================================
 # Activate services
-#--------------------------------------
 suseInsertService sshd
 
-#======================================
 # Setup default target, multi-user
-#--------------------------------------
 baseSetRunlevel 3
 
-#======================================
 # Remove yast if not in use
-#--------------------------------------
 #suseRemoveYaST
+
+# set plymouht theme
+plymouth-set-default-theme naiad
---
+++ alexandrite/root/etc/skel/.bashrc
@@ -0,0 +1,4 @@
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/share/powerline/bash/powerline.sh
